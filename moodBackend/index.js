8167b815f1a7abeee01c8cd4e8bc7394a72a8c28
ussyalfaks <usmanalfaki@gmail.com>
2024-11-05 23:21:14 +0100

    changes

--- moodBackend/index.js
+++ moodBackend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://mood-tracker-eta-two.vercel.app/',
+  origin: 'https://mood-tracker-eta-two.vercel.app',
   credentials: true
 }));
 app.use(express.json());
