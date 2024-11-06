0060e4208d1c7037c95649d3845c270524d3595d
ussyalfaks <usmanalfaki@gmail.com>
2024-11-06 01:41:52 +0100

    ort

--- moodBackend/index.js
+++ moodBackend/index.js
@@ -10,7 +10,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.get('/', ( res) => {
   res.send('App is running');
 });
