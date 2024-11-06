2a2105573af1fc9292c11be952365ae3d98560ad
ussyalfaks <usmanalfaki@gmail.com>
2024-11-06 01:39:46 +0100

    latest

--- mood/src/lib/api.jsx
+++ mood/src/lib/api.jsx
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://localhost:3000/api';
 
 export async function register(data) {
   const res = await fetch(`${API_URL}/auth/register`, {
