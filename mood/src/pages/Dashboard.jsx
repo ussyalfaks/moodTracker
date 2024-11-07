39e1c0bf6877cd869844ff0d20f7460a9c966efe
ussyalfaks <usmanalfaki@gmail.com>
2024-11-07 14:22:00 +0100

    latest

--- mood/src/pages/Dashboard.jsx
+++ mood/src/pages/Dashboard.jsx
@@ -1,5 +1,4 @@
-import { Link, useLocation } from 'react-router-dom'
-import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import Logo from '../assets/Logo.png'
 import Emotional from '../assets/emotional.png'
 import { EarthLock, Award } from 'lucide-react'
@@ -18,16 +17,17 @@ export function Dashboard ()  {
       <span className="text-xl font-semibold text-[#fff5DC] capitalize">MoodTracker</span>
     </Link> 
     
-    <div className="flex space-x-4 2">
-        <NavLink to="/" label="Article" />
-        <NavLink to="/" label="About" /> 
-        <NavLink to="/" label="Licenses" />
-    </div>
+    <div className='flex items-center'> 
+    {/* <div className="flex space-x-4 2">
+        <NavLink to="/" label="Design Inspiration" /> 
+        
+    </div> */}
 
     <div>
       <Link to="/register">
         <button className='bg-[#fff5DC] text-[#629878] mx-5 py-2 px-5 rounded-3xl font-semibold'>Register</button>
       </Link>
+    </div>
     </div>
 
    </div>
@@ -35,7 +35,7 @@ export function Dashboard ()  {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row items-center justify-between gap-12 pb-4 pt-10 md:py-12">
           <div className="flex-1 space-y-6">
-            <h1 className="font-serif text-4xl md:text-4xl lg:text-6xl font-semibold leading-tight tracking-wide text-center md:text-left text-gray-700">
+            <h1 className="font-serif text-3xl md:text-4xl lg:text-6xl font-semibold leading-tight tracking-wide text-center md:text-left text-gray-700">
               Track and Improve
               <span className="block mt-2">Your Emotional</span>
               <span className="block mt-2">Well-being</span>
@@ -51,7 +51,7 @@ export function Dashboard ()  {
                 Get Started
               </button>
               </Link>
-              <button className="border-2 border-[#629878] text-[#629878] px-8 py-3 rounded-3xl font-medium hover:bg-orange-50 transition-colors duration-200">
+              <button disabled  className=" cursor-not-allowed border-2 border-[#629878] text-[#629878] px-8 py-3 rounded-3xl font-medium hover:bg-orange-50 transition-colors duration-200">
                Learn More
               </button>
             </div>
@@ -171,27 +171,27 @@ export function Dashboard ()  {
 }
 
 
-function NavLink({ to,  label }) {
-  const location = useLocation();
-  const isActive = location.pathname === to;
+// function NavLink({ to,  label }) {
+//   const location = useLocation();
+//   const isActive = location.pathname === to;
 
-  return (
-    <Link
-      to={to}
-      className={`flex items-center py-1 px-2 my-2 rounded-full text-sm font-medium ${
-        isActive
-          ? 'text-[#629878] bg-[#fff5DC]'
-          : 'text-[#fff5DC] hover:text-white hover:border-r-2 hover:border-l-2'
-      }`}
-    >
+//   return (
+//     <Link
+//       to={to}
+//       className={`flex items-center py-1 px-2 my-2 rounded-full text-sm font-medium ${
+//         isActive
+//           ? 'text-[#629878] bg-[#fff5DC]'
+//           : 'text-[#fff5DC] hover:text-white hover:border-r-2 hover:border-l-2'
+//       }`}
+//     >
      
-      {label}
-    </Link>
-  );
-}
+//       {label}
+//     </Link>
+//   );
+// }
 
-NavLink.propTypes = {
-    to: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-}
+// NavLink.propTypes = {
+//     to: PropTypes.string.isRequired,
+//     label: PropTypes.string.isRequired,
+// }
 
