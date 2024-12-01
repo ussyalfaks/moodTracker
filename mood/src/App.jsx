2085839d514bb3068b17b0225e9705e6e02c8c55
ussyalfaks <usmanalfaki@gmail.com>
2024-12-01 16:15:12 +0100

    time

--- mood/src/App.jsx
+++ mood/src/App.jsx
@@ -20,7 +20,7 @@ function PrivateRoute({ children }) {
   useEffect(() => {
     const timer = setTimeout(() => {
       setDelayComplete(true);
-    }, 1000);
+    }, 100);
 
     return () => clearTimeout(timer);
   }, []);
